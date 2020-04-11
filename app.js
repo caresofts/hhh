ee09f07e951df2b082d86160207454eb1bb63fca
Mr.Dark Tech <59573521+caresofts@users.noreply.github.com>
2020-04-11 17:01:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var lock = navigator.mozSettings.createLock();
+lock.set({'developer.menu.enabled': true});
+lock.set({'debug.console.enabled': true});
+lock.set({'tethering.support': true});
+lock.set({'device.capability.vowifi': true});
